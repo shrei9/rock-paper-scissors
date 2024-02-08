c4ac257a87e4627f13caad2a92dc1bbfce27038a
Dan <kikero0101@gmail.com>
2024-02-08 21:21:54 +0200

    changed logic

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-let playerChoice, pcChoice;
+let playerChoice, computerChoice;
 const choice = [`rock`, `paper`, `scissors`];
 
-function getPcChoice(max, min) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 function getPlayerChoice() {
-    return parseInt(prompt("0 - rock\n1 - paper\n2-scissors", "0"));
+    let temp = prompt("Rock, paper or scissors?");
+    temp = temp.trim();
+    return temp.toLowerCase();
 }
 
-function play(playerSelection, computerSelection) {
-    if ((playerChoice === `paper` && pcChoice === `rock`) || (playerChoice === `scissors` && pcChoice === `paper`) || (playerChoice === `rock` && pcChoice === `scissors`)) {
+function play() {
+
+    playerChoice = getPlayerChoice();
+    computerChoice = choice[Math.floor(Math.random() * choice.length)]
+
+    if ((playerChoice === `paper` && computerChoice === `rock`) ||
+        (playerChoice === `scissors` && computerChoice === `paper`) ||
+        (playerChoice === `rock` && computerChoice === `scissors`)) {
         console.log("Player won");
     }
-    else if (playerChoice === pcChoice) console.log("draw");
+    else if (playerChoice === computerChoice) console.log("draw");
     else console.log("PC won");
 }
 
-playerChoice = choice[getPlayerChoice()];
-pcChoice = choice[getPcChoice(2, 0)];
-
-play(playerChoice, pcChoice);
+play();
 
 
 
